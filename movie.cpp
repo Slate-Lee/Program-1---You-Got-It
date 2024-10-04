09f8410acb5a8a1f27be6b51559bac75cdef6d25
TravisMehaffy <travis.mehaffy@gmail.com>
2024-10-03 22:14:19 -0500

    Update movie.cpp

--- movie.cpp
+++ movie.cpp
@@ -1,33 +1,63 @@
 #include "movie.h"
 
+// Default constructor
 Movie::Movie()
 {
     title = "";
     director = "";
     year = 0;
     rating = 0.0;
+    movieReview = nullptr;
 }
 
+// Parametrized constructor
 Movie::Movie(string tTitle, string tDirector, int tYear, float tRating)
 {
     title = tTitle;
     director = tDirector;
     year = tYear;
     rating = tRating;
-};
+    movieReview = nullptr;
+}
 
+// Destructor to free dynamically allocated Review
+Movie::~Movie()
+{
+    delete movieReview;
+}
+
+// Display movie and review information
 void Movie::display()
 {
     cout << "\nTITLE: " << title;
     cout << "\nDIRECTOR: " << director;
     cout << "\nDATE: " << year;
     cout << "\nRATING: " << rating << endl;
-};
+    if (movieReview)
+    {
+        movieReview->display(); // Display the review if it exists
+    }
+}
 
+// Save movie and review information to a file
 void Movie::save(ofstream &out)
 {
     out << title << "\n";
     out << director << "\n";
     out << year << "\n";
-    out << rating;
+    out << rating << "\n";
+    if (movieReview)
+    {
+        movieReview->save(out);
+    }
+}
+
+// Function to add or update a review for the movie
+void Movie::addReview(string reviewerName, string reviewText)
+{
+    if (movieReview)
+    {
+        delete movieReview;
+    }
+    movieReview = new Review(reviewerName, reviewText);
 }
